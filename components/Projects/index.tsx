8be0fa911031e3ee8a830ab4b5065be7131ec038
ghani <ghani.rizkymail@gmail.com>
2022-06-07 19:58:26 +0700

    project feautures

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -1,32 +1,29 @@
 import React from "react";
+import {
+  Pagination,
+  Scrollbar,
+  A11y,
+  EffectCoverflow,
+  Navigation,
+} from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+import myProjects from "../../public/files/project_features.json";
+import Link from "next/link";
 
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/effect-coverflow";
+import "swiper/css/scrollbar";
+
 import Image from "next/image";
 
 const Projects = () => {
-  const WIDTH = 1000;
-  const HEIGHT = 500;
+  const WIDTH = 1200;
+  const HEIGHT = 600;
 
-  const arrImage = [
-    {
-      title: "StoreGG",
-      url: "storegg.png",
-    },
-    {
-      title: "StoreGG-CMS",
-      url: "storegg-cms.png",
-    },
-    {
-      title : "Gamefeed Discord Bot",
-      url : "logo-discord.png"
-    },
-    {
-      title : "Check other on my Github",
-      url : "logo-github.jpg"
-    }
-  ];
+  const projectData = myProjects;
 
   return (
     <>
@@ -38,25 +35,45 @@ const Projects = () => {
                 <h2>Things I have designed and made for my portfolio</h2>
               </div>
               <Swiper
+                effect={"coverflow"}
+                grabCursor={true}
+                centeredSlides={true}
+                modules={[
+                  Pagination,
+                  Scrollbar,
+                  A11y,
+                  EffectCoverflow,
+                  Navigation,
+                ]}
                 spaceBetween={50}
                 slidesPerView={2}
-                onSlideChange={() => console.log("slide change")}
-                onSwiper={(swiper) => console.log(swiper)}
+                coverflowEffect={{
+                  rotate: 50,
+                  stretch: 0,
+                  depth: 100,
+                  modifier: 1,
+                  slideShadows: true,
+                }}
+                navigation={{ hideOnClick: true }}
+                scrollbar={{ draggable: true }}
+                pagination={{ type: "fraction" }}
               >
-                {arrImage.map((e) => {
+                {projectData.map((e) => {
                   return (
                     <SwiperSlide
-                      key={`${e.title}-${e.url}`}
+                      key={`${e.title}-${e.image}`}
                       className="img-project"
                     >
                       <p className="mx-auto my-3 font-weight-bold">{e.title}</p>
                       <hr />
-                      <Image
-                        src={`/images/project/${e.url}`}
-                        width={WIDTH}
-                        height={HEIGHT}
-                        alt="image"
-                      />
+                      <a href={e.url} target="_blank" rel="noreferrer">
+                        <Image
+                          src={`/images/project/${e.image}`}
+                          width={WIDTH}
+                          height={HEIGHT}
+                          alt="image"
+                        />
+                      </a>
                     </SwiperSlide>
                   );
                 })}
